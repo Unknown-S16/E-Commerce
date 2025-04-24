c6aac8a89fb224cc670988c0064f65319b22e3ad
Unknown-S16 <sharath20038@gmail.com>
2025-04-24 19:53:27 +0530

    Hosting links updated

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const { default: mongoose } = require("mongoose");
 
 const app=express();
 app.use(cors({
-  origin: "http://localhost:5173", 
+  origin: "https://e-commerce-ivory-two.vercel.app/", 
   credentials: true
 }));
 app.use(express.json());
